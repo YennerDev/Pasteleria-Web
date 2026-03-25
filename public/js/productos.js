b04bfd56de328c54dc292b0b387473404c81fb3f
YennerDev <yenner0008@gmail.com>
2026-03-25 00:08:12 -0500

    actualice producto

--- public/js/productos.js
+++ public/js/productos.js
@@ -1,46 +1,41 @@
-
-
 async function cargarProductos() {
-try {
-const response = await fetch("/api/productos");
-const productos = await response.json();
+  try {
+    const response = await fetch("/api/productos");
+    const productos = await response.json();
 
-const contenedor = document.getElementById("productos-container");
+    const contenedor = document.getElementById("productos-container");
+    //contenedor.innerHTML = "";
 
-// 🔥 Limpia SOLO los dinámicos (no afecta los fijos)
-contenedor.innerHTML = "";
+    productos.forEach(producto => {
 
-productos.forEach(producto => {
+      const card = `
+      <div class="producto">
+        <img src="${producto.imagen}" width="200">
+        <h3>${producto.nombre}</h3>
+        <p>S/ ${producto.precio}</p>
 
-  // 🔥 Corrige la ruta de la imagen
-  const rutaImagen = producto.imagen.startsWith("http")
-    ? producto.imagen
-    : `/img/${producto.imagen}`;
+        <button onclick="pedirProducto('${producto.nombre}')">
+        Pedir
+        </button>
 
-  const card = `
-  <div class="producto">
-    <img src="${rutaImagen}" width="200">
-    <h3>${producto.nombre}</h3>
-    <p>S/ ${producto.precio}</p>
+      </div>
+      `;
 
-    <button onclick="pedirProducto('${producto.nombre}')">
-      Pedir
-    </button>
-  </div>
-  `;
+      contenedor.innerHTML += card;
 
-  contenedor.innerHTML += card;
+    });
 
-});
-
-} catch (error) {
-console.error("Error cargando productos:", error);
-}
+  } catch (error) {
+    console.error("Error cargando productos:", error);
+  }
 }
 
 function pedirProducto(nombre){
-localStorage.setItem("productoSeleccionado", nombre);
-window.location.href = "pedidos.html";
+
+  localStorage.setItem("productoSeleccionado", nombre);
+
+  window.location.href = "pedidos.html";
+
 }
 
 cargarProductos();
